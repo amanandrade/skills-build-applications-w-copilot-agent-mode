3d9fdeb0e4c5dd24bd8c16efe8a9f2b245ea7ecd
Amanda Andrade <43591109+amanandrade@users.noreply.github.com>
2025-04-27 14:15:43 +0000

    updating

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
-        'NAME': 'monafit_db',
+        'NAME': 'octofit_db',
         'HOST': 'localhost',
         'PORT': 27017,
     }
